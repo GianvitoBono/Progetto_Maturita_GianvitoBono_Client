7fabd3b0fa4c1fa4dc5010d46aeb09244845fdfe
GianvitoBono <gianvitobono@gmail.com>
2017-06-11 17:09:56 +0200

    Finita MessageView
    TODO: Bug Fix, Sistema di salvataggio nel DB locale dei messaggi, Aggiustare listener nel ConnectorService con le notifiche, la scrittura dei messaggi nel DB e intent vari ;-)
    
    #130#SiVolaaaaaaaaaaaa

--- app/src/main/java/com/giansoft/cryptedchat/Chat.java
+++ app/src/main/java/com/giansoft/cryptedchat/Chat.java
@@ -4,17 +4,21 @@ import android.content.ComponentName;
 import android.content.Context;
 import android.content.Intent;
 import android.content.ServiceConnection;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
 import android.os.Handler;
 import android.os.IBinder;
 import android.os.Message;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.FrameLayout;
 import android.widget.ImageView;
 import android.widget.TextView;
+import com.github.bassaer.chatmessageview.models.User;
+import com.github.bassaer.chatmessageview.views.MessageView;
 
-import java.util.ArrayList;
 
 public class Chat extends AppCompatActivity {
 
@@ -24,6 +28,9 @@ public class Chat extends AppCompatActivity {
     private String tel;
     private SynchronizedQueue<Object> synchronizedQueue = new SynchronizedQueue<>();
     private TextView messageBox;
+    private MessageView mChatView;
+    private com.github.bassaer.chatmessageview.models.User me;
+    private com.github.bassaer.chatmessageview.models.User you;
 
     @Override
     protected void onStart() {
@@ -33,20 +40,12 @@ public class Chat extends AppCompatActivity {
     }
 
     @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        connectorService.comunicate(Utils.delIP(securePreferences.getString("tel")), this);
-        if (connectorService != null)
-            unbindService(serviceConnection);
-    }
-
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_chat);
         securePreferences = new SecurePreferences(this);
+
+
 
         Intent i = getIntent();
         String name = i.getStringExtra("name");
@@ -55,18 +54,42 @@ public class Chat extends AppCompatActivity {
         View chatBox = findViewById(R.id.chatBox);
         ImageView send = (ImageView) chatBox.findViewById(R.id.send);
         messageBox = (TextView) chatBox.findViewById(R.id.messageBox);
+        mChatView = (MessageView) findViewById(R.id.chatMessageView);
+        me = new User(0, "Io", null);
+        you = new User(1, name, null);
+
+
+        mChatView.setRightBubbleColor(ContextCompat.getColor(this, R.color.colorPrimaryDark));
+        mChatView.setLeftBubbleColor(ContextCompat.getColor(this, R.color.colorPrimary));
+        mChatView.setBackgroundColor(Color.WHITE);
+        mChatView.setRightMessageTextColor(Color.BLACK);
+        mChatView.setLeftMessageTextColor(Color.BLACK);
+        mChatView.setUsernameTextColor(Color.BLACK);
+        mChatView.setSendTimeTextColor(Color.BLACK);
+        mChatView.setMessageMarginTop(5);
+        mChatView.setMessageMarginBottom(5);
+
 
         send.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String message = messageBox.getText().toString();
+                final String message = messageBox.getText().toString();
                 messageBox.setText("");
                 if (message != null) {
                     connectorService.comunicate(tel, message, new Handler() {
                         @Override
                         public void handleMessage(Message msg) {
                             super.handleMessage(msg);
-                            System.out.println("Errore nella connessione");
+                            if(msg.arg1 == Utils.FAIL)
+                                System.out.println("Errore nella connessione");
+                            else {
+                                com.github.bassaer.chatmessageview.models.Message mess = new com.github.bassaer.chatmessageview.models.Message.Builder()
+                                        .setUser(me)
+                                        .setRightMessage(true)
+                                        .setMessageText(message)
+                                        .build();
+                                mChatView.setMessage(mess);
+                            }
                         }
                     });
                 }
@@ -88,7 +111,13 @@ public class Chat extends AppCompatActivity {
                     super.handleMessage(msg);
                     Bundle data = msg.getData();
                     String mess = data.getString("message");
-                    System.out.println(mess + "--------------------------------------------");
+
+                    com.github.bassaer.chatmessageview.models.Message message = new com.github.bassaer.chatmessageview.models.Message.Builder()
+                            .setUser(you)
+                            .setRightMessage(false)
+                            .setMessageText(mess)
+                            .build();
+                    mChatView.setMessage(message);
                 }
             });
         }
